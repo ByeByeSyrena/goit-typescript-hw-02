26933de202f50d94610b551c32537564b8fe5010
Ira S <iryna.shevchenko2@gmail.com>
2023-11-16 18:37:58 +0200

    baseTypes done

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,22 +2,35 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
-  title: 'The awesome page',
+
+type Page = {
+  title?: string;
+  name?: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date; 
+  };
+}
+
+const page1: Page = {
+  title: "Great page",
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+  status: "open",
   details: {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
   }
-}
+};
 
-const page2 = {
-  title: 'Python or Js',
+const page2: Page = {
+  name: "Python or Js",
   likes: 5,
   accounts: ['Alex'],
-  status: 'close',
-}
+  status: "close",
+};
 
 export {};
